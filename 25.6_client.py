024c75150d39af68ea917ebed2aee283b13bc2f5
mykhailo678 <mykhailo@knu.ua>
2026-03-04 11:44:19 +0200

    Clean up comments in 25.6_client.py
    Removed commented-out code for clarity.

--- 25.6_client.py
+++ 25.6_client.py
@@ -7,16 +7,13 @@ if os.path.exists(file_path):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(('localhost', 12346))
 
-    # Відправляємо назву файлу (тільки ім'я, без шляху)
     file_name = os.path.basename(file_path)
     client.send(file_name.encode('utf-8'))
 
-    # Невелика пауза, щоб сервер встиг обробити назву
     import time
 
     time.sleep(0.1)
 
-    # Відправляємо вміст файлу
     with open(file_path, 'rb') as f:
         while True:
             data = f.read(4096)
